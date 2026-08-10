4d97b6fe000e75b756653b247ba4bfa2834b4216
Sonic <sonicsmritshrestha@gmail.com>
2026-08-10 23:49:44 +0545

    changing links

--- frontend/script.js
+++ frontend/script.js
@@ -20,7 +20,7 @@ async function  getResponse() {
 
     try{
 
-        const response = await fetch("http://127.0.0.1:8000/predict")
+        const response = await fetch("https://studioartnet.onrender.com/predict")
         if (!response.ok) {
             throw new Error("API request failed");
         }
